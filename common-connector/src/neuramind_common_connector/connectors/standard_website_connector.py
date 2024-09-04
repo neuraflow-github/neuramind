fe0b4b53f7c0d1bdc3aeb17b1ee6fcd2ed2769d9
Julius Huck <mail@juliushuck.com>
2024-09-04 14:21:14 +0200

    Update

--- common-connector/src/neuramind_common_connector/connectors/standard_website_connector.py
+++ common-connector/src/neuramind_common_connector/connectors/standard_website_connector.py
@@ -3,7 +3,7 @@ from neuramind_common.models.website_chunk_model import WebsiteChunkModel
 from .base_connector import BaseConnector
 
 
-class StandardWebsiteChunksConnector(BaseConnector[WebsiteChunkModel]):
+class StandardWebsiteConnector(BaseConnector[WebsiteChunkModel]):
     def __init__(self):
         super().__init__()
 
